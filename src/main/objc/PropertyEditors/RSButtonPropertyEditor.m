9149ba64fc0757238a272b8474da3ee4809619c9
Todd Reed <todd.reed@reactionsoftware.com>
2015-09-26 16:29:39 -0600

    Set the color of the label of a RSButtonPropertyEditor according to whether an action is defined

--- src/main/objc/PropertyEditors/RSButtonPropertyEditor.m
+++ src/main/objc/PropertyEditors/RSButtonPropertyEditor.m
@@ -10,6 +10,15 @@
 
 @implementation RSButtonPropertyEditor
 
+#pragma mark - RSPropertyEditor
+
+- (nonnull UITableViewCell *)newTableViewCell
+{
+    return [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:nil];
+}
+
+#pragma mark - RSButtonPropertyEditor
+
 - (nonnull instancetype)initWithTitle:(nonnull NSString *)aTitle action:(void (^_Nullable)(RSObjectEditorViewController *))aAction
 {
     self = [super initWithKey:nil title:aTitle];
@@ -23,6 +32,7 @@
 - (void)configureTableCellForValue:(nullable id)value controller:(nonnull RSObjectEditorViewController *)controller
 {
     [super configureTableCellForValue:value controller:controller];
+    [self styleButtonLabel];
 }
 
 - (BOOL)selectable
@@ -34,6 +44,18 @@
 {
     if (_action)
         _action(controller);
+}
+
+- (void)setAction:(void (^)(RSObjectEditorViewController * _Nonnull))action
+{
+    _action = action;
+    [self styleButtonLabel];
+}
+
+- (void)styleButtonLabel
+{
+    UIColor *buttonColor = self.selectable ? self.tableViewCell.tintColor : [UIColor grayColor];
+    self.tableViewCell.textLabel.textColor = buttonColor;
 }
 
 @end
